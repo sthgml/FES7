97e7fd867069a7fe0d50b5179283bedbcefb3e9d
sohee <sthgml@naver.com>
2023-09-19 14:23:32 +0900

    ♻️ Update: 1만시간의 법칙 과제 기능구현까지

--- React/20230917/the-10000hrs-rule/src/App2.js
+++ React/20230917/the-10000hrs-rule/src/App2.js
@@ -2,14 +2,17 @@ import Header from "./components/header/header";
 import Main from "./components/main/main";
 import Footer from "./components/footer/footer";
 import Modal from "./components/modal/Modal";
+import { useState } from "react";
 
 function App2 () {
+    const [modalShow, setModalShow] = useState(false);
+
     return(
         <div>
             <Header />
-            <Main />
+            <Main setModalShow={setModalShow}/>
             <Footer />
-            <Modal />
+            {modalShow ?(<Modal setModalShow={setModalShow}/>) : null}
         </div>
     )
 }
